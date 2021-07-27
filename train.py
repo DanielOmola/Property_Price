cef31938c913d2a7f4b27e97d96297950c2446d5
Daniel Omola <43685205+DanielOmola@users.noreply.github.com>
2021-07-27 20:09:53 +0200

    Update train.py

--- train.py
+++ train.py
@@ -45,7 +45,7 @@ import platform
 
 #years = [2020,2019,2018,2017,2016,2015,2014]
 #years = [2020]
-years = [2020,2019,2018,2017]
+years = [2020,2019,2018,2017,2016]
 
 
 # --------- Chose the area with departement code
